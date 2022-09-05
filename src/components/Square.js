3f9f19a03af3eb72f9cc7103adc84c29adff838e
Aziz Magomedov <74148010+doggxmax@users.noreply.github.com>
2022-09-05 09:12:58 +0300

    Delete src directory

--- src/components/Square.js
+++
@@ -1,9 +0,0 @@
-import "./Square.css";
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-export default Square;
