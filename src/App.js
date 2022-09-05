394e20f51b76c3777179deab82c6980085eff01d
Aziz Magomedov <74148010+doggxmax@users.noreply.github.com>
2022-09-05 09:05:03 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from "./Login";
+import "./App.css";
+import Game from "./components/Game";
+function App() {
+  return (
+    <Login>
+      <Game></Game>
+    </Login>
+  );
+}
+export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import "./Square.css";
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+export default Square;
